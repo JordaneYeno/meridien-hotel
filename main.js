8715e0cc0b6c69e3cbed0fc4067c706f9bc27077
YENO ADEMBIGONDJO Jordane <adembigondjo@gmail.com>
2022-06-20 19:53:13 +0100

    'meridien'

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $("#txtdateentree").datepicker({
+        showOn: "button",
+
+        // Button image stored on local device
+        buttonImage: "./ressources/icons/info.png",      
+        buttonImageOnly: true
+    });
+});
+$(document).ready(function () {
+   $("#txtdatesortie").datepicker({
+        showOn: "button",
+
+        // Button image stored on local device
+        buttonImage: "./ressources/icons/info.png",    
+        buttonImageOnly: true
+    }); 
+});
+
+
+
+    
